17d7d3978c42f6065e0713a93114f52ddbbcf006
caicai789525 <146819268+caicai789525@users.noreply.github.com>
2026-01-04 16:54:11 +0800

    Delete backend directory

--- backend/service/document.go
+++
@@ -1,6 +0,0 @@
-package service
-
-import "github.com/bugoutianzhen123/SoftwareConstructionExp/domain"
-
-func (s *Service) AddDocument(d *domain.Document) (*domain.Document, error) { return s.repo.AddDocument(d) }
-func (s *Service) ListDocumentsByApplication(appID int64) []*domain.Document { return s.repo.ListDocumentsByApplication(appID) }
